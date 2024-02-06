4d038e09bb148581f4c9e6b2a73a1a72410bc83b
Developer <testmail.gmail.com>
2024-02-06 20:11:35 +0100

    Project structure

---
+++ src/components/Picture/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Picture() {
+
+    const text = 'Hallo...';
+
+    return (
+        <div>
+            <h1>{text}</h1>
+        </div>
+    );
+
+}
+
+export default Picture;
