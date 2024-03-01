0511ac8021497d9428cc3e19e910435b54b26ee9
Bergi <testmail.gmail.com>
2024-03-01 08:30:38 +0100

    Component Card created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Basket from './components/Basket';
 import Card from './components/Card';
 import Header from './components/Header';
 
+const arr = [
+  {
+    title: 'sdfdsfdsfdsfdsf',
+    price: 25,
+    url: '/img/pictures/01.jpg'
+  },
+  {
+    title: 'sdfdsfdsfdsfdsf',
+    price: 25,
+    url: '/img/pictures/02.jpg'
+  },
+  {
+    title: 'sdfdsfdsfdsfdsf',
+    price: 25,
+    url: '/img/pictures/03.jpg'
+  },
+  {
+    title: 'sdfdsfdsfdsfdsf',
+    price: 25,
+    url: '/img/pictures/04.jpg'
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -18,11 +41,18 @@ function App() {
           </div>
         </div>
 
-        <div className="d-flex justify-between">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="d-flex">
+          {
+            arr.map((obj) => (
+              <Card
+                title = {obj.title}
+                price = {obj.price}
+                url = {obj.url}
+                onLikeClick = {() => alert(12345)}
+                onAddClick = {() => alert(1234)} 
+              />
+            ))
+          }
         </div>
 
       </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import styles from './Card.module.scss';
 
-function Card() {
+console.log(styles);
 
-    const text = 'Hallo...';
+function Card(props) {
 
     return (
-        <div>
-            <h1>{text}</h1>
+        <div className={styles.card}>
+            <img className={styles.btnUnliked} onClick={props.onLikeClick} src="/img/heart-unliked.png" alt="Unliked" />
+            <img width={155} height={135} src={props.url} alt="Bild1" />
+            <h5>{props.title}</h5>
+            <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                    <span>Preis: </span>
+                    <b>{props.price}</b>
+                </div>
+                <button className="button" onClick={props.onAddClick}>
+                    <img className="btn-img" src="/img/plus.svg" alt="Plus" />
+                </button>
+            </div>
         </div>
     );
-
 }
 
 export default Card;
